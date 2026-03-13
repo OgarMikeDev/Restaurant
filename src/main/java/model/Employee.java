d5fa9e722cbc8359449a40f021e4a39a334138ed
OgarMikeDev <123175953+OgarMikeDev@users.noreply.github.com>
2026-03-13 09:48:13 +0300

    Update Employee.java

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -9,85 +9,96 @@ import java.util.Set;
 
 /*
 TODO
- Employee implements GenerateOfSalary -
- чтобы у каждого сотрудника формировалась зп.
+ abstract - Employee является вспомогательным,
+ его объекты создать нельзя!
  */
-public abstract class Employee implements GenerateOfSalary {
-    private String fullName;
-    private double monthSalary; //TODO месячная зп
-    private double сlientSalary; //TODO доход с 1-го клиента
+public abstract class Employee implements FormationSalary {
+    private String fio;
+    private int salary;
     private LocalDate dateStartWork;
-    //TODO static относится не к 1-го сотруднику, а сразу ко всем
-    private static double incomeRestaurant = 0; //TODO доход ресторана
+    //TODO static - не относится к 1-му конкретному сотруднику
+    private static int incomeRestaurant = 0;
     /*
     TODO
-      Set<Employee> employeeSet хранит только неповторяющихся сотрудников.
-      Их уникальность он отслеживает благодаря
-      equals(Object o) и
-      hashCode()
+     employeeSet хранит только неповторяющихся сотрудников.
+     new HashSet<>() не сортирует сотрудников по нужному нам полю
      */
     private static Set<Employee> employeeSet = new HashSet<>();
 
-    //TODO конструктор для создания текущего сотрудника(this)ЯЧ
-    public Employee(String fullName, double monthSalary, double сlientSalary, LocalDate dateStartWork) {
-        this.fullName = fullName;
-        this.monthSalary = monthSalary;
-        this.сlientSalary = сlientSalary;
+    //TODO Конструктор для установления значений fio, salary и dateStartWork
+    public Employee(String fio, int salary, LocalDate dateStartWork) {
+        this.fio = fio;
+        this.salary = salary;
         this.dateStartWork = dateStartWork;
-        incomeRestaurant += сlientSalary;
-        this.generateOfSalary(this);
-        employeeSet.add(this); //TODO this - текущий сотрудник
+        //TODO При создании каждого сотрудника, то, что он заработал переходит ресторану
+        incomeRestaurant += salary;
+        this.formationSalary(this);
+        //TODO Текущий сотрудник добавляется в employeeSet
+        employeeSet.add(this);
+    }
+
+    /*
+    TODO
+       equals(Object o) и hashCode()
+       позволяет employeeSet при добавлении сотрудников,
+       проверять значения всех переменных, т.е.
+       salary, fio, dateStartWork
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return salary == employee.salary && Objects.equals(fio, employee.fio) && Objects.equals(dateStartWork, employee.dateStartWork);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fio, salary, dateStartWork);
+    }
+
+    /*
+        TODO
+         Геттеры - для получения значений переменных.
+         Сеттеры - для установления значений переменным,
+         через проверку(контролируемое изменение значения)
+         */
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
     }
 
     public LocalDate getDateStartWork() {
         return dateStartWork;
     }
 
-    public void setСlientSalary(double сlientSalary) {
-        this.сlientSalary = сlientSalary;
+    public void setDateStartWork(LocalDate dateStartWork) {
+        this.dateStartWork = dateStartWork;
     }
 
-    public static double getIncomeRestaurant() {
+    public static int getIncomeRestaurant() {
         return incomeRestaurant;
     }
 
-    public double getСlientSalary() {
-        return сlientSalary;
+    public static Set<Employee> getEmployeeSet() {
+        return employeeSet;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Employee employee = (Employee) o;
-        return Double.compare(monthSalary, employee.monthSalary) == 0 && Double.compare(сlientSalary, employee.сlientSalary) == 0 && Objects.equals(fullName, employee.fullName) && Objects.equals(dateStartWork, employee.dateStartWork);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(fullName, monthSalary, сlientSalary, dateStartWork);
-    }
-
-    public static void printAllEmployee() {
-        for (Employee currentEmployee : employeeSet) {
-            System.out.println("Текущий сотрудник: " + currentEmployee);
-        }
-    }
-
-    public static void printIncomeRestaurant() {
-        System.out.println("Доход ресторана на текущий момент равен " + incomeRestaurant + " руб.");
-    }
-
-    /*
-    TODO
-      toString() позволяет при выводе сотрудника,
-      получать значения всех его переменых
-     */
     @Override
     public String toString() {
         return "Employee{" +
-                "fullName='" + fullName + '\'' +
-                ", monthSalary=" + monthSalary +
-                ", сlientSalary=" + сlientSalary +
+                "fio='" + fio + '\'' +
+                ", salary=" + salary +
                 ", dateStartWork=" + dateStartWork +
                 '}';
     }
